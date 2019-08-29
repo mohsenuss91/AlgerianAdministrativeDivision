1686706850cc60cf4646b755a1990506b1fbcf79
Abdel Hakim Bensiali <bensialih@gmail.com>
2019-08-29 22:27:33 +0100

    chaned schema & added co-ordinates of Wilayas & children

--- src/dataset/index.ts
+++ src/dataset/index.ts
@@ -2,12 +2,16 @@
 export type Baladiya = {
     arabic: string;
     french: string;
+    latitude: number;
+    longitude: number;
 }
 
 export type Daira = {
     arabic: string;
     french: string;
-    Baladiyas: Baladiya[]
+    Baladiyas: Baladiya[];
+    latitude: number;
+    longitude: number;
 }
 
 export type Wilaya = {
@@ -16,4 +20,6 @@ export type Wilaya = {
     matricule: string;
     phoneCodes: string[];
     Dairas: Daira[];
+    latitude: number;
+    longitude: number;
 }
